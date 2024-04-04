224fcb9ba90ae240efb83de772a0b33753fc41c9
IBFrost <126430979+IBFrost@users.noreply.github.com>
2024-04-03 20:48:45 -0400

    LoginScreen Minor changes
    Changed naming scheme to clarify what items are for when we implement properly later.

--- src/main/java/LoginScreen.java
+++ src/main/java/LoginScreen.java
@@ -13,25 +13,27 @@ public class LoginScreen {
         JFrame window = new JFrame("PMS System Prototype");
 
         window.setSize((int) Toolkit.getDefaultToolkit().getScreenSize().width * 3 / 4,
-                (int) Toolkit.getDefaultToolkit().getScreenSize().height * 3 / 4);
-        window.setLocationRelativeTo(null);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                (int) Toolkit.getDefaultToolkit().getScreenSize().height * 3 / 4); // Sets the window to 3/4 the size of the screen it is currently on.
+        window.setLocationRelativeTo(null); // Centralizes the window on the screen
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Assigns the close ([X] Button) action to fully exit the program.
 
-        JTextField textField1 = new JTextField(12);
-        JTextField textField2 = new JTextField(12);
+        JTextField usernameField = new JTextField(12);
+        JTextField passwordField = new JTextField(12);
 
-        JButton button = new JButton("Log In");
-        button.addActionListener(e -> {
+        JButton loginButton = new JButton("Log In");
+        loginButton.addActionListener(e -> {
             // TODO: Implement button/submission of credentials.
         });
 
-        // Create a JPanel to hold components
+        // Create JPanel + Add declared objects to it.
         JPanel loginPanel = new JPanel();
         loginPanel.add(new JLabel("Username:"));
-        loginPanel.add(textField1);
+        loginPanel.add(usernameField);
         loginPanel.add(new JLabel("Password:"));
-        loginPanel.add(textField2);
-        loginPanel.add(button);
+        loginPanel.add(passwordField);
+        loginPanel.add(loginButton);
+
+        // Establish layout schema and add JPanel to the window
         window.setLayout(new GridBagLayout());
         window.add(loginPanel, new GridBagConstraints());
         window.setVisible(true);
