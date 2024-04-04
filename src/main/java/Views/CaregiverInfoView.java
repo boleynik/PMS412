ba7e24707021ffd9ab62dec19e6415034c329336
Ben <131935756+boleynik@users.noreply.github.com>
2024-04-04 14:24:54 -0400

    cargiverinfo view added “Wherever the art of Medicine is loved, there is also a love of Humanity.” ― Hippocrates

---
+++ src/main/java/Views/CaregiverInfoView.java
@@ -0,0 +1,12 @@
+package Views;
+
+import javax.swing.*;
+
+public class CaregiverInfoView {
+    private JLabel userIDLabel;
+    private JLabel emailLabel;
+    private JTextField phoneNumberField;
+    private JButton phoneNumberButton;
+    private JTextField departmentField;
+    private JButton departmentButton;
+}
