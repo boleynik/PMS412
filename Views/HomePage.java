5ba8190af4e3436cf4c551e588f93cc2cf1e1e1d
Losdye <140551386+Losdye@users.noreply.github.com>
2024-04-07 23:03:13 -0400

    Add files via upload
    Added HomePage

---
+++ Views/HomePage.java
@@ -0,0 +1,49 @@
+package View;
+
+import Controller.MIController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomePage {
+    private JPanel panel1;
+    public JButton viewMedicalInformationButton;
+
+    public JButton logoutButton;
+
+    public JButton updateMedicalInformationButton;
+    public JButton quitButton;
+
+    public HomePage() {
+        MIController controller = new MIController(this);
+        // Attach action listeners to buttons
+        viewMedicalInformationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.viewMedicalInformationButtonClicked();
+            }
+        });
+
+
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.logoutButtonClicked();
+            }
+        });
+
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.quitButtonClicked();
+            }
+        });
+    }
+
+    // Getter for panel1
+    public JPanel getPanel1() {
+        return panel1;
+    }
+}
