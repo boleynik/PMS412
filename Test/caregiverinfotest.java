654195e2590d81cf7da507870c5d6df47bf92b5b
Ben <131935756+boleynik@users.noreply.github.com>
2024-04-07 22:11:59 -0400

    tried to fix caregiverinfotest.java imports

--- Test/caregiverinfotest.java
+++ Test/caregiverinfotest.java
@@ -1,11 +1,4 @@
-package testharness;
 
-import Controllers.CaregiverInfoController;
-import Models.Caregiver;
-import Models.Medication;
-import Models.Patient;
-import Models.Record;
-import Views.CaregiverInfoView;
 
 import java.util.ArrayList;
 
@@ -13,16 +6,16 @@ public class caregiverinfotest {
     public static void main(String[] args) {
 
         ArrayList<Record> patient1records = new ArrayList<>();
-        ArrayList<Medication> patient1meds = new ArrayList<>();
+        ArrayList<Models.Medication> patient1meds = new ArrayList<>();
 
-        ArrayList<Patient> caregiver1Patients = new ArrayList<>();
+        ArrayList<Models.Patient> caregiver1Patients = new ArrayList<>();
 
-        Caregiver caregiver1 = new Caregiver(101, "janedoe@gmail.com", "9122122212", "General Medicine", caregiver1Patients);
+        Models.Caregiver caregiver1 = new Models.Caregiver(101, "janedoe@gmail.com", "9122122212", "General Medicine", caregiver1Patients);
         
-        Patient patient1 = new Patient(001, "johnsmith@gmail.com", "8144444444", "2000-01-01", "M", 160, 6, patient1records, patient1meds);
+        Models.Patient patient1 = new Models.Patient(001, "johnsmith@gmail.com", "8144444444", "2000-01-01", "M", 160, 6, patient1records, patient1meds);
 
-        CaregiverInfoView view = new CaregiverInfoView();
-        CaregiverInfoController controller = new CaregiverInfoController(caregiver1, view);
+        Views.CaregiverInfoView view = new Views.CaregiverInfoView();
+        Controllers.CaregiverInfoController controller = new Controllers.CaregiverInfoController(caregiver1, view);
         view.setVisible(true);
 
     }
